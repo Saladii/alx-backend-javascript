c76d966f910ae57a57e0706e42be74d048f33297
Saladii <selehadinseid1@gmail.com>
2023-09-26 17:32:29 +0000

    update

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([
+    uploadPhoto(),
+    createUser(),
+  ]).then((messages) => {
+    console.log(`${messages[0].body} ${messages[1].firstName} ${messages[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
